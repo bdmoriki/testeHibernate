a6fa036650ab8129a21d4905684137304ae7a8de
bruno <dupim1@hotmail.com>
2017-06-04 19:29:07 -0300

    teste

---
+++ src/main/java/br/com/bruno/testeHibernate/model/Tarefa.java
@@ -0,0 +1,37 @@
+package br.com.bruno.testeHibernate.model;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Tarefa {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Temporal(TemporalType.DATE)
+	private Calendar dataFinalizacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Calendar getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+
+	public void setDataFinalizacao(Calendar dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+
+}
